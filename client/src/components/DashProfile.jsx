af38cfec25b30c0ac993d451cde5a04bcfe4bc4c
singh043 <kumarjatin431109@gmail.com>
2024-03-31 21:15:29 +0530

    ui for dashprofile

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,5 +1,80 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+
 export default function DashProfile() {
+  const { currentUser } = useSelector((state) => state.user);
+
+  const [formData, setFormData] = useState({
+    username: currentUser.username,
+    email: currentUser.email,
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => {
+      return { ...prev, [name]: value.trim() };
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>DashProfile</div>
-  )
+    <div className="w-full flex flex-col items-center">
+      <div className="max-w-lg p-3 w-full">
+        <h1 className="text-3xl font-semibold my-7 text-center">Profile</h1>
+        <form
+          className="flex flex-col items-center gap-5"
+          onSubmit={handleSubmit}
+        >
+          <div className="w-32 h-32 rounded-full overflow-hidden shadow-md cursor-pointer">
+            <img
+              src={currentUser.profilePicture}
+              alt="Profile Picture"
+              className="w-full h-full rounded-full object-cover border-4 border-[lightgray]"
+            />
+          </div>
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            spellCheck={false}
+            onChange={handleChange}
+            className="w-full h-10 border-2 border-black/40 rounded-md px-4 py-2 outline-blue-700"
+          />
+          <input
+            type="text"
+            name="email"
+            value={formData.email}
+            spellCheck={false}
+            onChange={handleChange}
+            className="w-full h-10 border-2 border-black/40 rounded-md px-4 py-2 outline-blue-700"
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            spellCheck={false}
+            onChange={handleChange}
+            className="w-full h-10 border-2 border-black/40 rounded-md px-4 py-2 outline-blue-700"
+          />
+          <button
+            className="border-none outline-none w-full h-10 font-semibold rounded-lg
+            bg-gradient-to-r from-cyan-500 to-purple-500 text-white p-[2px] px-[3px]"
+            type="submit"
+          >
+            <div className="w-full h-full rounded-md bg-white hover:bg-transparent hover:text-white text-black flex items-center justify-center">
+              Update
+            </div>
+          </button>
+        </form>
+        <div className="mt-5 flex justify-between text-red-500 font-semibold">
+          <span className="cursor-pointer">Delete Account</span>
+          <span className="cursor-pointer">Sign out</span>
+        </div>
+      </div>
+    </div>
+  );
 }
