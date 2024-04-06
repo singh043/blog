324f4014e6d8b7ed9c222a3be4de43af25581164
singh043 <kumarjatin431109@gmail.com>
2024-04-06 22:46:17 +0530

    image upload

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,5 +1,15 @@
-import { useState } from "react";
+/* eslint-disable no-unused-vars */
+import { useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import { app } from "./../firebase";
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
@@ -9,6 +19,12 @@ export default function DashProfile() {
     email: currentUser.email,
     password: "",
   });
+  const filePickerRef = useRef();
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  const [imageFileUploadingProgress, setImageFileUploadProgress] =
+    useState(null);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -17,8 +33,56 @@ export default function DashProfile() {
     });
   };
 
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+
+    var file_type = file.type;
+    file_type = file_type.toLowerCase();
+    file_type = file_type.substr(0, file_type.indexOf("/"));
+
+    setImageFileUploadProgress(null);
+    setImageFileUploadError(null);
+
+    if (file && file_type === "image") {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+      uploadImage(e.target.files[0]);
+    } else if (file_type !== "image") {
+      setImageFileUploadError("Please upload a image file.");
+    }
+    e.target.value = "";
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+  };
+
+  const uploadImage = async (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImageFileUploadProgress(progress.toFixed(0));
+      },
+      (error) => {
+        setImageFileUploadError("Could not upload image");
+        setImageFileUploadProgress(null);
+        setImageFile(null);
+        setImageFileUrl(null);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileUrl(downloadURL);
+          setImageFileUploadError(null);
+          setImageFileUploadProgress(null);
+        });
+      }
+    );
   };
 
   return (
@@ -29,13 +93,55 @@ export default function DashProfile() {
           className="flex flex-col items-center gap-5"
           onSubmit={handleSubmit}
         >
-          <div className="w-32 h-32 rounded-full overflow-hidden shadow-md cursor-pointer">
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleImageChange}
+            ref={filePickerRef}
+            hidden
+            className=""
+          />
+          <div
+            className="w-32 h-32 rounded-full overflow-hidden shadow-md cursor-pointer relative"
+            onClick={() => filePickerRef.current.click()}
+          >
+            {imageFileUploadingProgress && (
+              <CircularProgressbar
+                value={imageFileUploadingProgress || 0}
+                text={`${imageFileUploadingProgress}%`}
+                strokeWidth={3}
+                styles={{
+                  root: {
+                    width: "100%",
+                    height: "100%",
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                  },
+                  path: {
+                    stroke: `rgba(62, 152, 199, ${
+                      imageFileUploadingProgress / 100
+                    })`,
+                  },
+                }}
+              />
+            )}
             <img
-              src={currentUser.profilePicture}
+              src={imageFileUrl || currentUser.profilePicture}
               alt="Profile Picture"
-              className="w-full h-full rounded-full object-cover border-4 border-[lightgray]"
+              className={`w-full h-full rounded-full object-cover border-4 border-[lightgray]
+              ${
+                imageFileUploadingProgress &&
+                imageFileUploadingProgress < 100 &&
+                "opacity-60"
+              }`}
             />
           </div>
+          {imageFileUploadError && (
+            <div className="text-center mt-5 bg-red-300 p-2 rounded-md select-text w-[400px] ">
+              {imageFileUploadError}
+            </div>
+          )}
           <input
             type="text"
             name="username"
