d35ff1058bf4de23a6373067224387ee69a5f30b
singh043 <kumarjatin431109@gmail.com>
2024-04-11 18:05:14 +0530

    add admin functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -20,6 +20,10 @@ const userSchema = new mongoose.Schema(
             type: String,
             default: "https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049_640.png",
         },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
     }, 
     {timestamps: true}
 )
