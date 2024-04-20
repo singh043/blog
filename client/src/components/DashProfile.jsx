01239eb95df4e817e77dbecf6c232dfe9f75c0f5
singh043 <kumarjatin431109@gmail.com>
2024-04-20 21:21:03 +0530

    add upload post image functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -128,7 +128,7 @@ export default function DashProfile() {
   const uploadImage = async (file) => {
     setImageFileUploading(true);
     const storage = getStorage(app);
-    const fileName = new Date().getTime() + file.name;
+    const fileName = new Date().getTime() + '_' + file.name;
     const storageRef = ref(storage, fileName);
     const uploadTask = uploadBytesResumable(storageRef, file);
     uploadTask.on(
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -4,11 +4,56 @@ import { VscChromeClose } from "react-icons/vsc";
 import ClickAwayListener from "react-click-away-listener";
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { app } from "../firebase";
+import { CircularProgressbar } from 'react-circular-progressbar';
+import "react-circular-progressbar/dist/styles.css";
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 
 const CreatePost = () => {
 
-    const [open, setOpen] = useState(false);
+    const [file, setFile] = useState(null);
+    const [open, setOpen] = useState(false)
+    const [formData, setFormData] = useState({});
     const [selectedOption, setSelectedOption] = useState(null);
+    const [imageUploadError, setImageUploadError] = useState(false);
+    const [imageUploadProgress, setImageUploadProgress] = useState(null);
+
+    const handleUploadImage = async () => {
+        try {
+            if(!file) {
+                setImageUploadError('Please select an image');
+                return;
+            }
+            setImageUploadError(null);
+            setImageUploadProgress(null);
+
+            const storage = getStorage(app);
+            const fileName = new Date().getTime() + '_' + file.name;
+            const storageRef = ref(storage, fileName);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+            uploadTask.on(
+                'state_changed',
+                (snapshot) => {
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    setImageUploadProgress(progress.toFixed(0));
+                },
+                (error) => {
+                    setImageUploadError('Image upload failed');
+                    setImageUploadProgress(null);
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        setImageUploadProgress(null);
+                        setImageUploadError(null);
+                        setFormData({...formData, image: downloadURL})
+                    })
+                }
+            )
+        } catch (error) {
+            setImageUploadError('Image upload failed');
+            setImageUploadProgress(null);
+        }
+    }
 
     return (
         <div className="min-h-screen max-w-3xl mx-auto p-3">
@@ -78,9 +123,48 @@ const CreatePost = () => {
                     </ClickAwayListener>
                 </div>
                 <div className="flex justify-between items-center p-3 border-4 border-dotted border-teal-500">
-                    <input type="file" accept="image/*" className="outline-none rounded-[10px] text-[16px] bg-white cursor-pointer" />
-                    <button type="button" className="border-2 border-black rounded-md py-2 px-3 font-semibold">Upload image</button>
+                    <input type="file" accept="image/*" className="outline-none rounded-[10px] text-[16px] bg-white cursor-pointer" onChange={(e) => {
+                        setImageUploadError(null);
+                        setImageUploadProgress(null);
+
+                        const file = e.target.files[0]
+            
+                        var file_type = file.type;
+                        file_type = file_type.toLowerCase();
+                        file_type = file_type.substr(0, file_type.indexOf("/"));
+                    
+                        if (file_type !== "image") {
+                            setImageUploadError("Please upload a image file.");
+                            return;
+                        }
+                        setFile(e.target.files[0]);
+                        // e.target.value = '';
+                    }} />
+                    <button type="button" className={`rounded-md p-[1.5px] font-semibold bg-gradient-to-r from-cyan-500 to-purple-500 
+                    ${imageUploadProgress && 'cursor-not-allowed'} w-[130px]`}
+                    disabled={imageUploadProgress}
+                    onClick={handleUploadImage}>
+                        <div className={`bg-white ${imageUploadProgress ? "" : "hover:text-white hover:bg-transparent"} py-2 px-3 rounded-md`}>{
+                            imageUploadProgress ? <div className="">
+                                {`${imageUploadProgress}%`}
+                            </div> : 'Upload Image'
+                        }</div>
+                    </button>
                 </div>
+                {
+                    imageUploadError && (
+                        <div className="text-center mt-5 bg-red-300 p-2 rounded-md select-text w-full">
+                            {imageUploadError}
+                        </div>
+                    )
+                }
+                {formData.image && (
+                    <img 
+                        src={formData.image}
+                        alt="upload"
+                        className="w-full h-72 object-cover"
+                    />
+                )}
                 <ReactQuill theme="snow" placeholder="Write something...." required className="h-72 mb-12" />
                 <button type="submit" className="border-none outline-none w-full h-10 font-semibold rounded-lg bg-gradient-to-r from-cyan-500 to-purple-500 text-white">Publish</button>
             </form>
