54020a064d2a05fad8ec4da5763c17a354ab94bd
singh043 <kumarjatin431109@gmail.com>
2024-04-30 18:54:37 +0530

    complete Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,54 @@
-export default function Home() {
-  return <div className="min-h-screen">Home</div>;
+import { Link } from "react-router-dom";
+import CallToAction from './../components/CallToAction';
+import { useEffect, useState } from "react";
+import PostCard from './../components/PostCard';
+
+const Home = () => {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch(`/api/post/getPosts`);
+      const data = await res.json();
+      setPosts(data.posts);
+    }
+    fetchPosts();
+  }, [])
+
+  return (
+    <div className="">
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+        <h1 className="text-3xl font-bold lg:text-6xl">Welcome to my Blog</h1>
+        <p className="text-gray-500 text-xs sm:text-sm">Here you&apos;ll find a variety of articles and tutorials on topics such as web development, software engineering, and programming languages.</p>
+        <Link to='/search' className="text-xs sm:text-sm text-teal-500 font-bold hover:underline">
+          View all posts
+        </Link>
+      </div>
+      <div className="p-3 bg-amber-100 dark:bg-slate-700">
+        <CallToAction />
+      </div>
+      <div className="w-full p-3 flex flex-col gap-8 py-7">
+        {
+          posts && posts.length > 0 && (
+            <div className="flex flex-col gap-6">
+              <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
+              <div className="flex flex-wrap justify-center gap-4">
+                {
+                  posts.map((post) => (
+                    <PostCard key={post._id} post={post} />
+                  ))
+                }
+              </div>
+              <Link to={'/search'} className="text-lg hover:underline text-center text-teal-500">
+                View all post
+              </Link>
+            </div>
+          )
+        }
+      </div>
+    </div>      
+  )
 }
+
+export default Home;
